35781ed86b61fc178c05171e51de3ef0ca727b4f
François Mockers <mockersf@gmail.com>
2018-02-28 18:38:49 +0100

    wrap script result in __html

--- src/reducers/custom.js
+++ src/reducers/custom.js
@@ -4,12 +4,26 @@ import { RECEIVE_TEST_COMPONENT, RECEIVE_RESULT_COMPONENT } from "../actions/cus
 const custom = (state = { test_component: undefined, result_component: undefined }, action) => {
     switch (action.type) {
         case RECEIVE_TEST_COMPONENT:
+            if (action.script === undefined) {
+                return state;
+            }
             return Object.assign({}, state, {
-                test_component: action.script,
+                test_component: (test) => {
+                    return {
+                        __html: action.script(test),
+                    };
+                },
             })
         case RECEIVE_RESULT_COMPONENT:
+            if (action.script === undefined) {
+                return state;
+            }
             return Object.assign({}, state, {
-                result_component: action.script,
+                result_component: (result, spans) => {
+                    return {
+                        __html: action.script(result, spans),
+                    };
+                },
             })
         default:
             return state
