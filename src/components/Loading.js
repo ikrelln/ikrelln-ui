be5c62e320bda8fde2e519a4275ef9aa75ea97b9
François Mockers <mockersf@gmail.com>
2018-03-11 02:26:06 +0100

    manage a few error cases

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 export class Loading extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            overDelay: false,
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(
+            () => this.setState({overDelay: true}),
+            3000
+        );
+    }
+
     render() {
         return (
             <div style={{padding: "20px"}}>
                 <div className="loader"></div>
+                {this.state.overDelay 
+                    ? <div style={{margin: "2em", fontSize: "larger"}}>
+                        <div style={{margin: "1em"}}>This is getting too long.</div>
+                        <div style={{margin: "1em"}}>The data you requested is not available.</div>
+                    </div>
+                    : null
+                }
             </div>
         )
     }
--- src/reducers/testDetails.js
+++ src/reducers/testDetails.js
@@ -10,6 +10,24 @@ const testDetails = (state = { traces: [], tests: [] }, action) => {
                 traces: [...state.traces, action.trace],
             })
         case RECEIVE_TEST:
+            if (action.test.test.error === 'NotFound') {
+                return state;
+            }
+            if ((action.test.test_id === 'root') && (action.test.test.test_id !== action.test.test_id)) {
+                return Object.assign({}, state, {
+                    tests: [...state.tests, {
+                        test_id: 'root',
+                        children: [
+                            {
+                                id: action.test.test.test_id,
+                                name: action.test.test.name,
+                            },
+                        ],
+                        path: [],
+                        last_results: [],
+                    }],
+                })    
+            }
             return Object.assign({}, state, {
                 tests: [...state.tests, action.test.test],
             })
